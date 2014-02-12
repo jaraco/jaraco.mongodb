20de51c85df1c545c1e73718858b08d3af31ca50
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2014-02-12 20:16:06 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -27,12 +27,14 @@ __doc__ = """Improved alternative to official mongooplog utility."""
 doclines = __doc__.splitlines()
 
 setup(name="mongooplog-alt",
-      version="0.4",
+      py_modules=["mongooplog_alt"],
+      version="0.4.2",
       author="Aleksey Sivokon",
       author_email="aleksey.sivokon@gmail.com",
       maintainer="Aleksey Sivokon",
       maintainer_email="aleksey.sivokon@gmail.com",
-      url = "https://github.com/publishthis/mongooplog-alt",
+      url = "https://github.com/asivokon/mongooplog-alt",
+      download_url="http://github.com/asivokon/mongooplog-alt/archive/0.4.2.tar.gz",
       license="http://www.apache.org/licenses/LICENSE-2.0.html",
       platforms=["any"],
       keywords='mongodb, mongo, oplog, mongooplog',
