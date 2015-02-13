56100a1236d838706a8792c08ba45fda90ebeedc
Jason R. Coombs <jaraco@jaraco.com>
2015-02-13 11:29:13 -0500

    Extract _rep_index_info

--- jaraco/mongodb/testing.py
+++ jaraco/mongodb/testing.py
@@ -1,7 +1,12 @@
 import pprint
+import textwrap
 
 from .query import compat_explain
 
+
+def _rep_index_info(coll):
+    index_info = coll.index_information()
+    return "Indexes are:\n" + pprint.pformat(index_info)
 
 def assert_covered(cur):
     """
@@ -9,9 +14,11 @@ def assert_covered(cur):
     was covered by an index.
     """
     explanation = compat_explain(cur)
-    index_info = cur.collection.index_information()
-    report = (
-        "Query was not covered:\n" + pprint.pformat(explanation) +
-        "\nIndexes are:\n" + pprint.pformat(index_info))
+    tmpl = textwrap.dedent("""
+        Query was not covered:
+        {explanation}
+        """).lstrip()
+    report = tmpl.format(explanation=pprint.pformat(explanation))
+    report += _rep_index_info(cur.collection)
     docs = explanation['executionStats']['totalDocsExamined']
     assert docs == 0, report
