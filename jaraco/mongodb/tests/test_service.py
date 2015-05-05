009255f0e9503d01dfba058890d5eee095a42e39
Jason R. Coombs <jaraco@jaraco.com>
2015-05-05 16:21:25 -0400

    Port tests from yg.test

---
+++ jaraco/mongodb/tests/test_service.py
@@ -0,0 +1,30 @@
+import datetime
+
+import pymongo
+import pytest
+from jaraco import timing
+
+from jaraco.mongodb import service
+
+
+def test_MongoDBReplicaSet_writable():
+    rs = service.MongoDBReplicaSet()
+    rs.start()
+    try:
+        conn = pymongo.MongoClient(rs.get_connect_hosts())
+        conn.database.collection.save({'foo': 'bar'})
+    finally:
+        rs.stop()
+
+
+def test_MongoDBReplicaSet_starts_quickly():
+    pytest.skip("Takes 20-30 seconds")
+    sw = timing.Stopwatch()
+    rs = service.MongoDBReplicaSet()
+    rs.start()
+    try:
+        elapsed = sw.split()
+        limit = datetime.timedelta(seconds=5)
+        assert elapsed < limit
+    finally:
+        rs.stop()
