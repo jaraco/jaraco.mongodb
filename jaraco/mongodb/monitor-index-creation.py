0c06cd8703d9e16cc1861e40f27e216aa688a5ca
Jason R. Coombs <jaraco@jaraco.com>
2015-11-12 21:49:44 -0500

    Only execute script when invoked as such

--- jaraco/mongodb/monitor-index-creation.py
+++ jaraco/mongodb/monitor-index-creation.py
@@ -1,17 +1,21 @@
 import sys
 import time
+import re
 from jaraco.mongodb import helper
 
 def is_index_op(op):
-    return op.get('query', {}).get('createIndexes')
+	return op.get('query', {}).get('createIndexes')
 
-db = helper.connect_db(sys.argv[1])
+def run():
+	db = helper.connect_db(sys.argv[1])
 
-while True:
-    ops = db.current_op()['inprog']
-    index_op = next(filter(is_index_op, ops))
-    msg = index_op['msg']
-    name = index_op['query']['indexes'][0]['name']
-    msg = re.replace('Index Build (\(background\))?', name, msg)
-    print(msg, end='\r')
-    time.sleep(5)
+	while True:
+		ops = db.current_op()['inprog']
+		index_op = next(filter(is_index_op, ops))
+		msg = index_op['msg']
+		name = index_op['query']['indexes'][0]['name']
+		msg = re.replace('Index Build(\ (background\))?', name, msg)
+		print(msg, end='\r')
+		time.sleep(5)
+
+__name__ == '__main__' and run()
