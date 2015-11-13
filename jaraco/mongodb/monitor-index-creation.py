52994e51b0f09dc9f90bc560091fac4dd878b988
Jason R. Coombs <jaraco@jaraco.com>
2015-11-12 21:36:18 -0500

    update script to use local modules

--- jaraco/mongodb/monitor-index-creation.py
+++ jaraco/mongodb/monitor-index-creation.py
@@ -1,11 +1,11 @@
 import sys
 import time
-import yg.mongodb
+from jaraco.mongodb import helper
 
 def is_index_op(op):
     return op.get('query', {}).get('createIndexes')
 
-db = yg.mongodb.connect_db(sys.argv[1])
+db = helper.connect_db(sys.argv[1])
 
 while True:
     ops = db.current_op()['inprog']
