f7eef6d39b4a8779d481b9fdf19d12b09421e092
Jason R. Coombs <jaraco@jaraco.com>
2017-05-01 16:06:32 -0400

    Add test capturing expectation that save should only replace a document when an _id is present. Ref #11.

--- jaraco/mongodb/tests/test_compat.py
+++ jaraco/mongodb/tests/test_compat.py
@@ -33,3 +33,14 @@ def test_save_replace_by_id(database):
 	compat.save(database.test_coll, doc)
 	assert database.test_coll.count() == 1
 	assert database.test_coll.find_one() == doc
+
+
+def test_save_no_id_extant_docs(database):
+	"""
+	When no id is supplied, a new document should be created.
+	"""
+	doc = dict(foo='bar')
+	compat.save(database.test_coll, dict(doc))
+	assert database.test_coll.count() == 1
+	compat.save(database.test_coll, doc)
+	assert database.test_coll.count() == 2
