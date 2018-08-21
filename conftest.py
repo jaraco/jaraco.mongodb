73ca4a748c25aa6f6c6969a78a1ab87c76714f6d
Jason R. Coombs <jaraco@jaraco.com>
2018-08-21 12:05:38 -0400

    prefer tabs

--- conftest.py
+++ conftest.py
@@ -6,15 +6,15 @@ import pytest
 collect_ignore = ['jaraco/mongodb/pmxbot.py']
 
 if six.PY2:
-    collect_ignore.append('jaraco/mongodb/monitor-index-creation.py')
+	collect_ignore.append('jaraco/mongodb/monitor-index-creation.py')
 
 
 @pytest.fixture(scope='function')
 def database(request, mongodb_instance):
-    """
-    Return a clean MongoDB database suitable for testing.
-    """
-    db_name = request.node.name.replace('.', '_')
-    database = mongodb_instance.get_connection()[db_name]
-    yield database
-    database.client.drop_database(db_name)
+	"""
+	Return a clean MongoDB database suitable for testing.
+	"""
+	db_name = request.node.name.replace('.', '_')
+	database = mongodb_instance.get_connection()[db_name]
+	yield database
+	database.client.drop_database(db_name)
