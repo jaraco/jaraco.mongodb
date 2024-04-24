691819b7a37a54cc3b565a836a989aff6af1ce6c
Jason R. Coombs <jaraco@jaraco.com>
2024-04-24 16:21:48 -0400

    Re-use run context.

--- jaraco/mongodb/tests/test_service.py
+++ jaraco/mongodb/tests/test_service.py
@@ -10,25 +10,19 @@ from jaraco.mongodb import service
 @pytest.mark.xfail(reason="#31")
 def test_MongoDBReplicaSet_writable():
     rs = service.MongoDBReplicaSet()
-    rs.start()
-    try:
+    with rs.run():
         conn = pymongo.MongoClient(rs.get_connect_hosts())
         conn.database.collection.insert_one({'foo': 'bar'})
-    finally:
-        rs.stop()
 
 
 def test_MongoDBReplicaSet_starts_quickly():
     pytest.skip("Takes 20-30 seconds")
     sw = timing.Stopwatch()
     rs = service.MongoDBReplicaSet()
-    rs.start()
-    try:
+    with rs.run():
         elapsed = sw.split()
         limit = datetime.timedelta(seconds=5)
         assert elapsed < limit
-    finally:
-        rs.stop()
 
 
 def test_fixture(mongodb_instance):
